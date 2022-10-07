0a54a0ed4e35042d3fedae8de25945c5abfe2530
maeng0830 <kmk930830@gmail.com>
2022-10-07 20:12:41 +0900

    project: hello-spring. implement registerMember, memberList

---
+++ hello-spring/src/main/java/maeng0830/hellospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package maeng0830.hellospring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
