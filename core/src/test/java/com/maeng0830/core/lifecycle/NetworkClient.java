a2cafac79e0e4a6e70b141e603016323e46c1ac2
maeng0830 <kmk930830@gmail.com>
2022-12-20 16:23:55 +0900

    Feat: InitializingBean, DisposableBean 방법
    1. 단점
      - 스프링 전용 인터페이스이다.
      - 초기화, 소멸 메서드 이름 변경 X
      - 내가 코드를 고칠 수 없는 외부 라이브러리에 적용할 수 없다.
      - 거의 사용하지 않는다.

--- core/src/test/java/com/maeng0830/core/lifecycle/NetworkClient.java
+++ core/src/test/java/com/maeng0830/core/lifecycle/NetworkClient.java
@@ -1,12 +1,13 @@
 package com.maeng0830.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
     }
 
     public void setUrl(String url) {
@@ -25,5 +26,20 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect() {
         System.out.println("close " + url);
+    }
+
+    // 의존 관계 주입 후 호출
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    // 빈 소멸 직전 호출
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
