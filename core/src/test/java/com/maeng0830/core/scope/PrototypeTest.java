8d02b3dd77a3c0994281a384231429abd12c95b7
maeng0830 <kmk930830@gmail.com>
2022-12-21 12:59:38 +0900

    Feat: PrototypeBean
    - 특징
      - 스프링 컨테이너에 해당 빈을 요청할 때마다 새로운 객체로 생성된다.
      - 스프링 컨테이너는 빈의 생성, 의존관계 주입, 초기화 단계 까지만 관여한다.
      - 그 이후로는 클라이언트가 관리해야한다. 따라서 종료 메서드도 클라이언트가 직접 호출해야한다.

---
+++ core/src/test/java/com/maeng0830/core/scope/PrototypeTest.java
@@ -0,0 +1,40 @@
+package com.maeng0830.core.scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class); // 프로토타입 빈은 빈을 요청할 때 생성된다. 매번 다른 객체로..
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        ac.close(); // 스프링 컨테이너는 프로토타입 빈을 생성까지만 할 뿐, 그 이후로는 관리하지 않는다. 소멸 메소드가 호출되지 않는다.
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
